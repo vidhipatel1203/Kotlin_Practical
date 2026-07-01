1afba9767aece9ea9bd8b126d561c57fdd703649
patelvidhi <patelvidhivasai2006@gmail.com>
2026-07-01 09:09:27 +0530

    p11 p12

---
+++ src/P12.kt
@@ -0,0 +1,32 @@
+fun main(){
+
+    val i:Int
+    i=10
+
+    val j:Double
+    j=i.toDouble()
+
+    val k:String
+    k="10"
+
+    val l:Int
+    l=k.toInt()
+
+    val m:Int
+    m=k.toInt()
+
+    val o:String
+    o="10.2"
+
+    val n:Double
+    n=o.toDouble()
+
+    println("i="+i)
+    println("j="+j)
+    println("k="+k)
+    println("l="+l)
+    println("m="+m)
+    println("o="+o)
+    println("n="+n)
+
+}
--- src/p11.kt
+++ src/p11.kt
@@ -1,4 +1,39 @@
 fun main(){
-    var i=10
+    var i:Int
+    i=22
+    var f:Float
+    f=1.5F
+    var c: Char
+    c='T'
+    var str: String
+    str="HMS"
+    var d: Double
+    d=96.36
+    var b: Boolean
+    b=true
+    var l: Long
+    l=338927847
+    var s: Short
+    s=-2
+    var m: Byte
+    m=127
+    var j: Int
+    j=45
+
+
+
     println("i="+i)
+    println("f="+f)
+    println("c="+c)
+    println("str="+str)
+    println("d="+d)
+    println("b="+b)
+    println("l="+l)
+    println("s="+s)
+    println("m="+m)
+    println("j=$j")
+
+
+
+
 }
